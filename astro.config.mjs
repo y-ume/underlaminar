113af9080b1671cddad991b6387f39881f1e7a6b
underlaminar <zp4421@gmail.com>
2025-12-31 21:46:17 +0800

    https->http

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://y-ume.github.io",
+	site: "http://y-ume.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
